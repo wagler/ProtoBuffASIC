43e44e75d12e1cdc48d5d1d35106c71728a93451
Gil <adhason@umich.edu>
2021-11-23 14:34:30 -0500

    Clock

--- top_level.sv
+++ top_level.sv
@@ -1,3 +1,4 @@
+`timescale 10ns/1ns
 module top_level(
     /*** APB3 BUS INTERFACE ***/
 	/*
@@ -14,10 +15,11 @@ module top_level(
 	*/
 
    	input clk,
+	input reset,
 	input [63:0] value,
 	input [4:0] field_type,
 	input [28:0] field_id,
-	output logic [119:0] out_port
+	output logic [1:0][119:0] out_port 
 
     /*** I/O PORTS DECLARATION ***/	
 ); 
@@ -28,17 +30,37 @@ module top_level(
     assign PREADY = 1;
 	*/
 
+	logic i, next_i;
+	assign next_i = i + 1;
+
+	wire [1:0][119:0] next_out_port;
+	wire [119:0] mux_out_port;
+
     top_varint tv1(
 		.value(value),
 	  	.field_type(field_type),
-	 	.out_port(out_port[119:40])       
+	 	.out_port(mux_out_port[119:40])
 	);
 
 	field_header fh1(
 		.field_id(field_id),
 		.field_type(field_type),
-		.out_port(out_port[39:0])
+		.out_port(mux_out_port[39:0])
 	);
+
+	always_ff @(posedge clk)
+	begin
+		if (reset)
+		begin
+			i = 0;
+			out_port = 240'b0;
+		end
+		else
+		begin
+				i <= next_i;
+				out_port[i] <= mux_out_port;
+		end
+	end
 
 
 endmodule
--- top_level_tb.sv
+++ top_level_tb.sv
@@ -1,13 +1,24 @@
+`timescale 10ns/1ns
 module top_level_tb;
 
 	logic clk;
+	logic reset;
 	logic [63:0] value;
 	logic [28:0] field_id;
 	logic [4:0] field_type;
-	logic [119:0] out_port;
+	logic [1:0][119:0] out_port;
+
+    initial
+    begin
+        clk = 0;
+    end
+
+    always
+        #1 clk = !clk;
 
 	top_level tl1(
 			.clk(clk),
+			.reset(reset),
 			.value(value),
 			.field_id(field_id),
 			.field_type(field_type),
@@ -19,6 +30,25 @@ module top_level_tb;
 
 		$monitor("@%g value = %d \t field_id = %d \t field_type = %d \t out_port = %h", $time, value, field_id, field_type, out_port);
 
+		@(negedge clk)
+		reset = 1;
+		@(negedge clk)
+		reset = 0;	
+		@(negedge clk)
+
+		value = 64'd150;
+		field_id = 29'd1;
+		field_type = 5'd5;
+
+		@(negedge clk)
+
+		value = -2;
+		field_id = 29'd2;
+		field_type = 5'd18;
+
+		@(negedge clk)
+
+/*
 		value = 64'd150;
 		field_id = 29'd1;
 		field_type = 5'd5;
@@ -42,6 +72,8 @@ module top_level_tb;
 		field_type = 5'd18;
 
 		#20
+
+*/
 
 		$finish;
 
