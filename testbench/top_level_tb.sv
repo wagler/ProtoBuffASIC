763919180a82e45b20db2f839bee8b56deb47411
Rohan Wagle <rwagle@umich.edu>
2021-12-07 20:17:06 -0500

    basic test1 works

--- testbench/top_level_tb.sv
+++ testbench/top_level_tb.sv
@@ -50,8 +50,8 @@ module top_level_tb;
 	initial
 	begin
 
-	$monitor("@%g dram_en: %b, done: %b, tl1.ob_out_entry_valid = %b, tl1.ser_done = %b, dram_addr = %h, tl1.ob_cpp_base_addr = %h, ob_out_entry = %h, data_to_dram = %h, tl1.sa.state = %b, tl1.sa.vs_en = %b, draw_rdwr = %b, dram.state2 = %b, dram_en_int = %b", $time, dram_en, done, tl1.ob_out_entry_valid, tl1.ser_done, dram_addr, tl1.ob_cpp_base_addr, tl1.ob_out_entry, data_to_dram, tl1.sa.state, tl1.sa.vs_en, dram_rdwr, dram.state2, dram.en_int);
-
+	$monitor("@%g dram_en: %b, done: %b, tl1.ob_out_entry_valid = %b, tl1.ser_done = %b, dram_addr = %h, tl1.ob_cpp_base_addr = %h, ob_out_entry = %h, data_to_dram = %h, tl1.sa.state = %b, tl1.sa.vs_en = %b, draw_rdwr = %b, dram.state2 = %b, dram_en_int = %b, data_from_dram=%h, fetch_out_entry=%h, fetch_out_valid=%b, dram_valid=%b", $time, dram_en, done, tl1.ob_out_entry_valid, tl1.ser_done, dram_addr, tl1.ob_cpp_base_addr, tl1.ob_out_entry, data_to_dram, tl1.sa.state, tl1.sa.vs_en, dram_rdwr, dram.state2, dram.en_int, data_from_dram, tl1.fetch_out_entry, tl1.fetch_out_valid, dram_valid);
+/*
 	reset = 1;
 	@(negedge clk);
 	reset = 0;
@@ -76,6 +76,52 @@ module top_level_tb;
 	while (~done) @(negedge clk);
 	@(negedge clk);
 
+
+	for (int i = 64'h300; i >= 64'h2f0; i = i - 1)
+		$display("mem[%h] = %h", i, dram.mem[i]);
+
+*/
+	reset = 1;
+    en = 0;
+	@(negedge clk);
+	reset = 0;
+	@(negedge clk);
+	@(negedge clk);
+
+	$readmemh("tests/test1/cpp_obj.mem", dram.mem, 64'h100);
+	$readmemh("tests/test1/table1.mem", dram.mem);
+
+    dram.mem[64'h200] = 8'hdd;
+    dram.mem[64'h201] = 8'hdd;
+    dram.mem[64'h202] = 8'hdd;
+    dram.mem[64'h203] = 8'hdd;
+    dram.mem[64'h204] = 8'hdd;
+    dram.mem[64'h205] = 8'hdd;
+    dram.mem[64'h206] = 8'hdd;
+    dram.mem[64'h207] = 8'hdd;
+    dram.mem[64'h208] = 8'h52;
+    dram.mem[64'h209] = 8'h6f;
+    dram.mem[64'h20A] = 8'h68;
+    dram.mem[64'h20B] = 8'h61;
+    dram.mem[64'h20C] = 8'h6e;
+
+	for (int i = 64'h200; i <= 64'h20C; i = i + 1)
+		$display("mem[%h] = %h", i, dram.mem[i]);
+
+	for (int i = 64'h100; i <= 64'h130; i = i + 1)
+		$display("mem[%h] = %h", i, dram.mem[i]);
+
+	for (int i = 64'h0; i <= 64'h30; i = i + 1)
+		$display("mem[%h] = %h", i, dram.mem[i]);
+
+	@(negedge clk);
+    en = 1;
+	@(negedge clk);
+	@(negedge clk);
+	@(negedge clk);
+	@(negedge clk);
+	while (~done) @(negedge clk);
+	@(negedge clk);
 
 	for (int i = 64'h300; i >= 64'h2f0; i = i - 1)
 		$display("mem[%h] = %h", i, dram.mem[i]);
